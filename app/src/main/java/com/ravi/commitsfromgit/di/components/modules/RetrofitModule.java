7286fa3cc1ad078d29aba9064cd71622cae5301f
Ravinder Katla <ravinder.katla@gmail.com>
2019-07-16 19:25:50 +0530

    Implementation of Pojos

--- app/src/main/java/com/ravi/commitsfromgit/di/components/modules/RetrofitModule.java
+++ app/src/main/java/com/ravi/commitsfromgit/di/components/modules/RetrofitModule.java
@@ -27,7 +27,6 @@ public class RetrofitModule {
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(okHttpClient)
                 .build();
-
     }
 
     @Provides
